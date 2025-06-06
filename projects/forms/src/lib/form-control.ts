d887efa1c546d91e2d0c285059293120be31354f
Érica Filipa Dias Cunha <erica@connecting-software.com>
2025-06-06 17:23:18 +0100

    Fix Status

--- projects/forms/src/lib/form-control.ts
+++ projects/forms/src/lib/form-control.ts
@@ -17,7 +17,9 @@ import {
 export class FormControl<T = any, V extends object = ValidatorsModel> extends NativeFormControl {
   override readonly value: ExtractControlValue<T>;
   override readonly valueChanges: Observable<ExtractControlValue<T>>;
-  override readonly status: Status;
+   override get status(): Status {
+    return super.status as Status;
+  };
   override readonly statusChanges: Observable<Status>;
   override readonly errors: ValidationErrors<V> | null;
 
